8e3e579f37d1ef905e6e2c69f93c2917ecf36fea
Kagurazaka Bokutake <me@mail.bokutake.com>
2018-05-27 14:36:41 +0800

    增加图片懒加载

--- footer.php
+++ footer.php
@@ -2,3 +2,15 @@
 <?php $this->footer(); ?>
 </body>
 </html>
+<?php
+    $echo = ob_get_contents();
+    ob_clean();
+    $placeholder = $this->options->themeUrl."/img/Akkarin.jpg";
+    if(!empty($this->options->lazyLoad)) {
+      $placeholder = $this->options->lazyLoad;
+    }
+    $preg = "/<img (.*)src(.*) \/>/i";
+    $replaced = '<img class="lazyload" \\1src="'.$placeholder.'" data-src\\2 />';
+    print preg_replace($preg, $replaced, $echo);
+    ob_end_flush();
+?>
