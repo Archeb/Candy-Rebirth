a9394e4a67a0c30226058f8f9e89782c04e407b4
Kanade <andy_wang0625@hotmail.com>
2020-06-22 08:00:00 -0400

    Update article-list.php
    Allow user to add their own Js code snippet like analytics and etc. before loading post pages

--- article-list.php
+++ article-list.php
@@ -6,14 +6,22 @@ if (!defined('__TYPECHO_ROOT_DIR__'))
 
         <?php function listArticles($art){
             $delay=0;
+			print<<<EOF
+			<script lang="javascript">
+				function showArticleWithJs(permalink){
+					//Your script goes here
+					showArticle(permalink);
+				}
+			</script>
+			EOF;
             while($art->next()):
             $delay++; ?>
             <?php if ($art->fields->previewImage && $art->fields->previewImage!==""): ?>
             <div class="article-with-preview right-in-animation" style="animation-duration:<?php echo 0.5+$delay*0.1?>s">
-                <div class="cover" onclick="showArticle('<?php $art->permalink() ?>')">
+                <div class="cover" onclick="showArticleWithJs('<?php $art->permalink() ?>')">
                     <div class="cover-image" style="background-image:url(<?php $art->fields->previewImage(); ?>)"><div class="title"><?php $art->title(); ?></div></div>
                 </div>
-                <div class="content" onclick="showArticle('<?php $art->permalink() ?>')">
+                <div class="content" onclick="showArticleWithJs('<?php $art->permalink() ?>')">
                     <div class="text">
                         <?php $art->excerpt(50, '') ?>
                     </div>
@@ -27,11 +35,11 @@ if (!defined('__TYPECHO_ROOT_DIR__'))
             </div>
             <?php else: ?>
             <div class="article right-in-animation">
-                <div class="title"><a onclick="showArticle('<?php $art->permalink() ?>')"><?php $art->title(); ?></a>
+                <div class="title"><a onclick="showArticleWithJs('<?php $art->permalink() ?>')"><?php $art->title(); ?></a>
                     <div class="meta"><?php array_map(function($v){echo '<a class="category" onclick="showArchive(\''.$v['permalink'].'\')" style="'.$v['description'].'">'.$v['name'].'</a>';},$art->categories) ?> <?php $art->author(); ?> · <?php echo time_elapsed_string('@'.$art->created) ?>前</div>
                 </div>
                 
-                <div class="content" onclick="showArticle('<?php $art->permalink() ?>')">
+                <div class="content" onclick="showArticleWithJs('<?php $art->permalink() ?>')">
                     <?php $art->content(); ?>
                 </div>
             </div>
